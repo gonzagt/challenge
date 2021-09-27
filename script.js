1eacfe9037940c726e3bf924b8cddf1888690b94
Gonzalo <gonzalogermantello@gmail.com>
2021-09-27 18:17:26 -0300

    agregada carpeta de iconos(s.u.t.), añadida y testeada la funcionalidad de los botones.

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("hello world");
+// Seleccionando elementos
+
+const balance = document.getElementById('saldoActual');
+const montoIngreso = document.getElementById('ingreso_monto');
+const montoEgreso = document.getElementById('egreso_monto');
+const razonIngreso = document.getElementById('ingreso_tipo');
+const razonEgreso = document.getElementById('egreso_tipo');
+const botonIngreso = document.getElementById('ingreso_boton');
+const botonEgreso = document.getElementById('egreso_boton');
+
+// Eventos
+
+let lista_movimientos = [];
+
+botonIngreso.addEventListener('click', function() {
+    let ingreso = {
+        propiedad: 'ingreso',
+        monto: parseFloat(montoIngreso.value),
+        razon: razonIngreso.value
+    }
+
+    console.log(ingreso.monto);
+    console.log(ingreso.propiedad);
+});
+
+botonEgreso.addEventListener('click', function () {
+    let egreso = {
+        propiedad: 'egreso',
+        monto: parseFloat(montoEgreso.value),
+        razon: razonEgreso.value
+    }
+    console.log(egreso.monto);
+    console.log(egreso.propiedad);
+});
+
