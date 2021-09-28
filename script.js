f355c5509670fe6d64eafb5d95b2ea0111b32d00
Gonzalo <gonzalogermantello@gmail.com>
2021-09-28 19:51:23 -0300

    added a lot of logic, operations now working and HTML being written from the script.js

--- script.js
+++ script.js
@@ -19,15 +19,15 @@ let balance = 0;
 let incomes = 0;
 let expenses = 0;
 
-// Events
+// Event Listeners
 
 incBtn.addEventListener('click', function() {
     if (!incAmount.value || !incType.value) return;
     let income = {
-        property: 'income',
+        property: 'Income',
         amount: parseFloat(incAmount.value),
         type: incType.value
-    }
+    };
     MOVEMENTS_LIST.push(income)
     refreshUI();
     clearInputs([incAmount, incType]);
@@ -36,10 +36,10 @@ incBtn.addEventListener('click', function() {
 expBtn.addEventListener('click', function () {
     if (!expAmount.value || !expType.value) return;
     let outcome = {
-        property: 'outcome',
+        property: 'Expense',
         amount: parseFloat(expAmount.value),
         type: expType.value
-    }
+    };
     MOVEMENTS_LIST.push(outcome)
     refreshUI();
     clearInputs([expAmount, expType]);
@@ -48,28 +48,50 @@ expBtn.addEventListener('click', function () {
 // Functions
 
 const refreshUI = () => {
-    incomes = calculateTotal('income', MOVEMENTS_LIST);
-    expenses = calculateTotal('outcome', MOVEMENTS_LIST);
-    balance = calculateBalance(incomes, expenses);
+    incomes = calculateTotal('Income', MOVEMENTS_LIST);
+    expenses = calculateTotal('Expense', MOVEMENTS_LIST);
+    balance = Math.abs(calculateBalance(incomes, expenses));
+    
+    let $sign = (incomes > expenses) ? "$" : "-$";
+
+    deleteEl([lastMovements]);
+    
+    MOVEMENTS_LIST.forEach((movement,index) => {
+        if (movement.property=="Income") {
+            addML(MOVEMENTS_LIST, movement.property, movement.amount, movement.type, index)
+        } else if (movement.property=="Expense") {
+        addML(MOVEMENTS_LIST, movement.property, movement.amount, movement.type, index)
+    }
+    })
+
+    finalBalance.innerHTML = `${$sign}${balance}`;
+    incTotal.innerHTML = `$${incomes}`;
+    expTotal.innerHTML = `$${expenses}`;
 }
 
 const calculateTotal = function(property, list) {
     let sum = 0;
     list.forEach(entry => {
-        if (entry.property == property) {
-            sum += entry.amount;
-        }
-    })
+        if (entry.property == property) {sum += entry.amount};
+    });
     return sum;
 }
 
-const calculateBalance = function (inc, exp) {
-    return inc - exp;
-}
+const calculateBalance = (inc, exp) => {return inc - exp};
 
-const clearInputs = (array) => {array.forEach(element => {
-    element.value = "";    
-});
-}
+const deleteEl = (elems) => {elems.forEach((elem) => { elem.innerHTML = "" })};
 
+const clearInputs = (array) => {array.forEach(element => {element.value = ""})};
+
+const addML = function(list, property, amount, type, id) {
+   const movement = `<li id="${id}" class="${property}">
+                        <p class="movement">${type}: ${amount}</p>
+                        <img id="edit" />
+                        <img id="delete" />
+                    </li>`;
+
+    const position = "afterbegin";
+
+    lastMovements.insertAdjacentHTML(position, movement);
+}
 
